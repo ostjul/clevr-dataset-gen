d0c1ddb208cab529146f1acfd8ee01e83741fc85
Julian Ost <julian.ost@algolux.com>
2023-04-17 21:26:15 -0400

    better looping script

--- image_generation/loop_image_gen.sh
+++ image_generation/loop_image_gen.sh
@@ -4,9 +4,14 @@
 blender_path="/home/pci/Downloads/blender-2.78c-linux-glibc219-x86_64/blender"
 output_image_dir="../CLEVR_2obj_w_pose/images/new/"
 
-start_idx = ls $output_image_dir | wc -l
+i=0
+j=1
+end=18
 
-while true
+while [ $i -lt $end ]
 do
-    timeout 600 $blender_path --background --python render_images.py -- --num_images 50000 --use_gpu 1 --width 256 --height 256 --camera_jitter 0.0 --min_objects 2 --max_objects 2 --output_image_dir ../CLEVR_2obj_w_pose/images/new/ --output_scene_dir ../CLEVR_2obj_w_pose/scenes/new/ --output_scene_file ../CLEVR_2obj_w_pose/scenes/CLEVR_new_scenes.json --start_idx start_idx
+        start_idx=$(ls $output_image_dir | wc -l)
+        echo $start_idx
+        i=$(( $i +  $j ))
+        timeout 600 $blender_path --background --python render_images.py -- --num_images 50000 --use_gpu 1 --width 256 --height 256 --camera_jitter 0.0 --min_objects 2 --max_objects 2 --output_image_dir $output_ima>
 done
